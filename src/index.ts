1265eb5d17e30adbaecbc32820e4f8cc4badc9ea
Wataru Ono <wtr0414@gmail.com>
2021-01-14 20:46:16 +0900

    配列とタプル

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,15 @@
 // console.log('Function parameters sample 5:', sum)
 
 // 05. オブジェクトの型定義
-import objectSample from './object/object'
-import typeAliasSample from './object/alias'
+// import objectSample from './object/object'
+// import typeAliasSample from './object/alias'
+//
+// objectSample()
+// typeAliasSample()
 
-objectSample()
-typeAliasSample()
+// 06. 配列とタプルの定義
+import arraySample from './array/array'
+import tupleSample from './array/tuple'
+
+arraySample()
+tupleSample()
