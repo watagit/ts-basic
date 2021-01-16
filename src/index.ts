89a9ffa97279035d425e05c04db9451742bc8a83
Wataru Ono <wtr0414@gmail.com>
2021-01-17 01:57:20 +0900

    ジェネリック型

--- src/index.ts
+++ src/index.ts
@@ -31,8 +31,15 @@
 // typeAliasSample()
 
 // 06. 配列とタプルの定義
-import arraySample from './array/array'
-import tupleSample from './array/tuple'
+// import arraySample from './array/array'
+// import tupleSample from './array/tuple'
+//
+// arraySample()
+// tupleSample()
 
-arraySample()
-tupleSample()
+// 07. ジェネリック型とポリモーフィズム
+import genericsBasicSample from './generics/basic'
+import genericsAdvancedSample from './generics/advanced'
+
+genericsBasicSample()
+genericsAdvancedSample()
